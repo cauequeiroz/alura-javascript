efad828b59b0389bcaf68f041b258039dd95bf5b
Caue Queiroz <cauenqueiroz@gmail.com>
2017-07-09 20:34:34 -0300

    Finish section 02

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,27 @@
 // Change tittle
 document.querySelector('.titulo').textContent = "Caue Queiroz";
+
+// Calculate IMC
+var paciente = document.querySelector('#primeiro-paciente');
+var elemImc  = paciente.querySelector('.info-imc');
+
+var altura = paciente.querySelector('.info-altura').textContent;
+var peso   = paciente.querySelector('.info-peso').textContent;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if ( peso <= 0 || peso >= 1000 ) {
+    elemImc.textContent = "Peso inválido!";
+    pesoValido = false;
+}
+
+if ( altura <= 0 || altura >= 3 ) {
+    elemImc.textContent = "Altura inválida!";
+    alturaValida = false;
+}
+
+if ( pesoValido && alturaValida ) {
+    var imc = peso / (altura * altura);
+    elemImc.textContent = imc;
+}
